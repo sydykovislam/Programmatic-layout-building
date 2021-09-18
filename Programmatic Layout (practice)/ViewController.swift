e719c158333ab5c3c62be216732214b2674d6e06
sydykovislam <sydykovislam03@gmail.com>
2021-09-18 22:01:02 +0600

    First commit

---
+++ Programmatic Layout (practice)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic Layout (practice)
+//
+//  Created by Sydykov Islam on 9/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
